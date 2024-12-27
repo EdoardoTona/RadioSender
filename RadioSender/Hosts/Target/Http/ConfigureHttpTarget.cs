18d357215cffbe957a96b89601048e8fe56bfe3f
Edoardo Tona <tona.edoardo@gmail.com>
2024-12-27 18:58:26 +0100

    +HttpTarget

---
+++ RadioSender/Hosts/Target/Http/ConfigureHttpTarget.cs
@@ -0,0 +1,45 @@
+﻿using Hangfire;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using RadioSender.Hosts.Common.Filters;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace RadioSender.Hosts.Target.Http;
+
+public record HttpTargetConfiguration : FilterableConfiguration
+{
+  public string? Url { get; set; }
+  public bool EnsureSuccessStatusCode { get; set; }
+}
+
+public static class ConfigureHttpTarget
+{
+  public static IHostBuilder ToSirap(this IHostBuilder builder)
+  {
+    builder.ConfigureServices((context, services) =>
+    {
+      if (!context.Configuration.GetValue("Target:HTTP:Enable", false))
+        return;
+
+      var clients = context.Configuration.GetSection("Target:HTTP:Targets").Get<IEnumerable<HttpTargetConfiguration>>();
+
+      foreach (var client in clients)
+      {
+        services.AddSingleton<ITarget>(sp =>
+          new HttpTarget(
+            sp.GetServices<IFilter>(),
+            sp.GetRequiredService<IHttpClientFactory>(),
+            sp.GetRequiredService<IBackgroundJobClient>(),
+            client
+            )
+        );
+      }
+
+    });
+
+    return builder;
+  }
+}
+
---
+++ RadioSender/Hosts/Target/Http/HttpTarget.cs
@@ -0,0 +1,77 @@
+﻿using Hangfire;
+using RadioSender.Helpers;
+using RadioSender.Hosts.Common;
+using RadioSender.Hosts.Common.Filters;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RadioSender.Hosts.Target.Http;
+
+public class HttpTarget : ITarget
+{
+
+  private readonly IBackgroundJobClient _backgroundJobClient;
+  private IFilter _filter = Filter.Invariant;
+  private HttpTargetConfiguration _configuration;
+  private static IHttpClientFactory? _httpClientFactory;
+
+  public HttpTarget(
+    IEnumerable<IFilter> filters,
+    IHttpClientFactory httpClientFactory,
+    IBackgroundJobClient backgroundJobClient,
+    HttpTargetConfiguration configuration)
+  {
+    _configuration = configuration;
+    _httpClientFactory = httpClientFactory;
+    UpdateConfiguration(filters, configuration);
+    _backgroundJobClient = backgroundJobClient;
+  }
+  public void UpdateConfiguration(IEnumerable<IFilter> filters, Configuration configuration)
+  {
+    Interlocked.Exchange(ref _configuration!, configuration as HttpTargetConfiguration);
+    Interlocked.Exchange(ref _filter, filters.GetFilter(_configuration.Filter));
+  }
+  public Task SendDispatches(IEnumerable<PunchDispatch> dispatches, CancellationToken ct = default)
+  {
+    foreach (var dispatch in dispatches)
+      SendDispatch(dispatch, ct);
+
+    return Task.CompletedTask;
+  }
+  public Task SendDispatch(PunchDispatch dispatch, CancellationToken ct = default)
+  {
+    if (dispatch.Punches == null)
+      return Task.CompletedTask;
+
+    var punches = _filter.Transform(dispatch.Punches);
+
+    foreach (var punch in punches)
+    {
+      _backgroundJobClient.Enqueue(() => SendPunchAction(_configuration, punch, default));
+    }
+    return Task.CompletedTask;
+  }
+
+
+  public static async Task SendPunchAction(HttpTargetConfiguration _configuration, Punch punch, CancellationToken ct = default)
+  {
+    if (string.IsNullOrEmpty(_configuration.Url) || _httpClientFactory == null)
+      throw new ArgumentException("Missing url");
+
+    using var httpClient = _httpClientFactory.CreateClient();
+
+    var url = _configuration.Url.Contains("localhost") ? _configuration.Url.Replace("localhost", "127.0.0.1") : _configuration.Url; // optimization to skip the dns resolution
+
+    url = FormatStringHelper.GetString(punch, url);
+
+    var response = await httpClient.GetAsync(url, ct);
+
+    if (_configuration.EnsureSuccessStatusCode)
+      response.EnsureSuccessStatusCode();
+
+  }
+
+}
